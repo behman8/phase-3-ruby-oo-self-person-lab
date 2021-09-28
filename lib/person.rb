e3e98bef83dc12f585f775c6663fd5a93b5f8e05
Branden <brandenehman45@gmail.com>
2021-09-27 18:15:26 -0600

    Still working through tasks

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
-# your code goes here
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account
+
+    def initialize name
+        @name = name
+    end
+
+    def bank_account
+        @bank_account = 25
+    end
+
+    def happiness
+        @happiness
+    end
+
+    def happiness
+        @happiness = 8
+        if @happiness(0..10)
+            @happiness
+        end
+    end
+end
